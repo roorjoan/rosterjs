0c09cd337fb1f8bcef249e283c8a2fdf52d8ee29
José A Rodríguez <roorjoan@gmail.com>
2023-04-04 00:01:41 +0200

    new commit
    +fixed drawCalendar function
    +module data.js
    +refactoring the project

--- app.js
+++ app.js
@@ -1,48 +1,69 @@
+import { months, pos, days30, colors } from "./data.js";
+
 window.addEventListener("DOMContentLoaded", () => {
-  let days = document.querySelectorAll(".day");
-
-  //pinta el calendario
   drawCalendar();
-
-  //pinta los dias de trabajo
-  markWorkDay(days);
+  //console.log(setFirst());
+  markWorkDay();
 });
 
-const markWorkDay = (days) => {
-  let yuliesky = "lightcoral";
-  let ricardo = "orange";
-  let jose = "lightgreen"; //verde
-  let carlos = "lightblue";
-
-  for (let index = 0; index < days.length; index += 4) {
-    days[index].style.background = ricardo;
-    days[index + 1].style.background = jose;
-    days[index + 2].style.background = carlos;
-    days[index + 3].style.background = yuliesky;
-  }
-};
-
 const drawCalendar = () => {
-  const months = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre",
-  ];
-  const pos = [7, 3, 3, 6, 1, 4, 6, 2, 5, 7, 3, 5];
   const my_date = new Date();
+  const my_month = my_date.getMonth();
 
-  document.getElementById("month").textContent = months[my_date.getMonth()];
+  document.getElementById("month").textContent = months[my_month];
 
   document
     .getElementById("first_day")
-    .style.setProperty("grid-column-start", pos[my_date.getMonth()]);
+    .style.setProperty("grid-column-start", pos[my_month]);
+
+  if (months[my_month] != "Febrero") {
+    const list = document.querySelector("ol");
+    if (days30.includes(months[my_month])) {
+      const li29 = document.createElement("li");
+      const li30 = document.createElement("li");
+      li29.textContent = 29;
+      li30.textContent = 30;
+      li29.classList.add("day");
+      li30.classList.add("day");
+      list.appendChild(li29);
+      list.appendChild(li30);
+    } else {
+      const li29 = document.createElement("li");
+      const li30 = document.createElement("li");
+      const li31 = document.createElement("li");
+      li29.textContent = 29;
+      li30.textContent = 30;
+      li31.textContent = 31;
+      li29.classList.add("day");
+      li30.classList.add("day");
+      li31.classList.add("day");
+      list.appendChild(li29);
+      list.appendChild(li30);
+      list.appendChild(li31);
+    }
+  }
+};
+
+const markWorkDay = () => {
+  const days = document.querySelectorAll(".day");
+
+  for (let i = 0; i < days.length; i += 4) {
+    days[i].style.background = colors[0];
+    days[i + 1].style.background = colors[1];
+    days[i + 2].style.background = colors[2];
+    days[i + 3].style.background = colors[3];
+  }
+};
+
+const setFirst = (array, firstElement = "yuliesky") => {
+  let array_aux = [];
+  let array_aux1 = [];
+  let aux = "";
+
+  for (let i = 0; i < 4; i++) {
+    array_aux[i] = array[i];
+    if (array[i] === firstElement) {
+      
+    }
+  }
 };
---
+++ data.js
@@ -0,0 +1,22 @@
+export const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+export const pos = [7, 3, 3, 6, 1, 4, 6, 2, 5, 7, 3, 5];
+export const days30 = [months[3], months[5], months[8], months[10]];
+export const colors = [
+  "lightcoral", //yuliesky
+  "orange", //ricardo
+  "lightgreen", //jose
+  "lightblue", //carlos
+];
